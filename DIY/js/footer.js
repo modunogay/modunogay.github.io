706aa9be95d064701e3671630e6bb549b22f17db
modunogay <modunogay@gmail.com>
2020-05-25 03:00:29 +0900

    change_CARD_src
    カードのソースが間違っていたので

--- DIY/js/footer.js
+++ DIY/js/footer.js
@@ -3,5 +3,5 @@ function footer(){
 	<a href="../FTR/index.html">家具<br>チェッカー</a>
 	<a href="../MISC/index.html">小物家具<br>チェッカー</a>
 	<a href="../FTR_WALL/index.html">壁掛け家具<br>チェッカー</a>`
-	//$("#Footer").append(add);
+	$("#Footer").append(add);
 }
