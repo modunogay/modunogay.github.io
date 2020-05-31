9ca52834d1958446d966dabe6667f04f70942143
modunogay <modunogay@gmail.com>
2020-05-31 20:30:37 +0900

    Change_HeaderComment
    文字を左寄せに

--- DIY/js/HeaderFooter.js
+++ DIY/js/HeaderFooter.js
@@ -9,16 +9,14 @@ function header(){
 function headerComment(){
 	add = `5/31 15:55更新<br>
 	<image src="../image/caution-01.png" style="width: 100px; height: 100px;"></image><br>
-	DIYレシピチェッカーに<span style="background-color: #ff9dc3b8;">「枚数」</span>を編集出来る機能を追加しました。<br><br>
-	この枚数は<span style="background-color: #ff9dc3b8;">コピペが出来ません。</span><br>
-	また、<span style="background-color: #ff9dc3b8;">枚数の共有もされません。</span><br><br>
-	そこで、シェアボタンから<span style="background-color: #ff9dc3b8;">「表示しているアイテム名をコピー」</span>する機能を実装しました。<br>
-	（もちろんフィルタ機能、並び替え機能にも対応しております。）<br><br>
-	さらに、DIYレシピチェッカーでは<span style="background-color: #ff9dc3b8;">「枚数」を表示</span>させた状態で名前をコピーすると、<br>
-	<span style="background-color: #ff9dc3b8;">自動的に名前の後ろに枚数(数字)</span>が付きます。<br>
+	<p id = "headerPlanText">DIYレシピチェッカーに<span style="background-color: #ff9dc3b8;">「枚数」</span>を編集出来る機能を追加しました。<br><br>
+	この枚数は<span style="background-color: #ff9dc3b8;">コピペが出来ません。</span>また、<span style="background-color: #ff9dc3b8;">枚数の共有もされません。</span><br><br>
+	そこで、シェアボタンから<span style="background-color: #ff9dc3b8;">「表示しているアイテム名をコピー」</span>する機能を実装しました。（もちろんフィルタ機能、並び替え機能にも対応しております。）<br><br>
+	特に、DIYレシピチェッカーでは<span style="background-color: #ff9dc3b8;">「枚数」を表示</span>させた状態で名前をコピーすると、<span style="background-color: #ff9dc3b8;">自動的に名前の後ろに枚数(数字)</span>が付きます。<br>
 	(もし枚数を初期化したい場合ページ一番下より可能です。)<br><br>
 	遅くなりましたが、<span style="background-color: #ff9dc3b8;">「家具チェッカー」</span>も作成いたしました。ページ上部からアクセスできます。<br>
-	今後ともよろしくお願い致します。@modunogay
+	今後ともよろしくお願い致します。</p>
+	<span style="text-align: right; display: inline-block; width: 90%;">@modunogay</span>
 	`
 	$("#HeaderComment").append(add);
 }
