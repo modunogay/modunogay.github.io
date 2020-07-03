22f74216cc29fcc9bd4ab0b8b02a737f0c9d0058
modunogay <modunogay@gmail.com>
2020-07-04 06:14:23 +0900

    addReact

---
+++ React_test/precache-manifest.bf71639fbc47c5c3cd8e6d45091108bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9662b486d6c1486c22622d0e7612492",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f21fd50d7e1dd7bdd207",
+    "url": "./static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "1673c08b1383a6359d0a",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f21fd50d7e1dd7bdd207",
+    "url": "./static/js/2.004db971.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.004db971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1673c08b1383a6359d0a",
+    "url": "./static/js/main.92270bc2.chunk.js"
+  },
+  {
+    "revision": "a236a167ba6e1e00c077",
+    "url": "./static/js/runtime-main.3a13c53f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
